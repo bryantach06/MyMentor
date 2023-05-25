6c910731085a89c0f178667398a7fc6eb441fcff
bryantach06 <108451443+bryantach06@users.noreply.github.com>
2023-05-26 01:00:33 +0700

    onboarding screen

---
+++ app/src/main/java/com/capstone/mymentor/ui/onboarding/WelcomeActivity.kt
@@ -0,0 +1,45 @@
+package com.capstone.mymentor.ui.onboarding
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.capstone.mymentor.databinding.ActivityWelcomeBinding
+import com.capstone.mymentor.ui.login.LoginActivity
+
+class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+        setupAction()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun setupAction() {
+        binding.btnStartGrowing.setOnClickListener {
+            val intent = Intent(this@WelcomeActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
